7d4baf6e7450baacf810890687c1d56d4b8c8eef
Ivan Mavrazas <mavrazas@gmail.com>
2018-07-26 19:43:02 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ivan Mavrazas on 7/26/18.
+//  Copyright © 2018 Ivan Mavrazas. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+}
